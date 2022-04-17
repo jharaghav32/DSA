a0005741ec7b36cb8cba3e34483972d6123b28dd
jharaghav <jharaghav3209@gmail.com>
2022-04-17 21:21:09 +0530

    a6

---
+++ sheet/array/a6.cpp
@@ -0,0 +1,8 @@
+// problem link:https://leetcode.com/problems/next-permutation/
+class Solution {
+public:
+    
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());
+    }
+};
