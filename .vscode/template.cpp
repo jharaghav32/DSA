c0eec5d3241785cf28b49ac474e4767bfdfc19d0
jharaghav <jharaghav3209@gmail.com>
2022-04-21 12:37:02 +0530

    template added

---
+++ .vscode/template.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+const int Mod = 1'000'000'007;
+const int N = 3e5, M = N;
+ll lcm(int a,int b){
+    ll a1=a;
+    ll b1=b;
+    return a1*b1/__gcd(a1,b1);
+}
+void solved(){
+    
+}
+int main(){
+ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    int t;
+    cin>>t;
+    while(t--){
+        solved();
+    }
+return 0;
+}
