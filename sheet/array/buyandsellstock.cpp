383d0aae7a9ff8b5d38b5bb72204b1ce2bbf1afd
jharaghav <jharaghav3209@gmail.com>
2022-05-07 23:38:37 +0530

    a18

---
+++ sheet/array/buyandsellstock.cpp
@@ -0,0 +1,13 @@
+// problem link:https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/submissions/
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//      int n = prices.size();
+//         int profit=0;
+//         for(int i=1;i<n;i++){
+//             if(prices[i]>prices[i-1])
+//                 profit += prices[i]-prices[i-1];
+//         }
+//         return profit;
+//     }
+// };
