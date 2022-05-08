3b5367714c79c6e47d5d14ef2ec93ce9e12cb22d
jharaghav <jharaghav3209@gmail.com>
2022-05-08 19:15:01 +0530

    4 sum

---
+++ sheet/array/4sum.cpp
@@ -0,0 +1,42 @@
+// problem link:https://leetcode.com/problems/4sum/submissions/
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n =nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        for(int i=0;i<n-3;i++){
+
+            
+            for(int j=i+1;j<n-2;j++){
+                
+                int sum =nums[i]+nums[j];
+                int low = j+1;
+                int high=n-1;
+                while(low<high){
+                    if(nums[low]+nums[high]==target-sum){
+                        vector<int> temp;
+                        temp.push_back(nums[i]);
+                        temp.push_back(nums[j]);
+                        temp.push_back(nums[low]);
+                        temp.push_back(nums[high]);
+                        ans.push_back(temp);
+                        while(low<high && nums[low]==nums[low+1])low++;
+                        while(low<high && nums[high]==nums[high-1])high--;
+                        low++;
+                        high--;
+                    }
+                    else if(nums[low]+nums[high]<target-sum)
+                        low++;
+                    else
+                        high--;
+                }
+                while(j+1<n && nums[j]==nums[j+1])j++;
+                
+            }
+            while(i+1<n && nums[i]==nums[i+1])i++;
+            }
+        
+        return ans;
+    }
+};
