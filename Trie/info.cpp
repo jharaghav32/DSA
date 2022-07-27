4decd892714389c3a5a72408ca84f51b174e0acb
jharaghav32 <jharaghav3209@gmail.com>
2022-07-27 21:33:23 +0530

    Trie

---
+++ Trie/info.cpp
@@ -0,0 +1,10 @@
+// Trie is an efficient information retrieval data structure 
+// which contains two data member
+// one is an array of alpha size of type trie class
+// and another one is boolean flag
+// syntax of tries is : 
+
+class Trie{
+    Trie* a[26];
+    bool flag;
+};
